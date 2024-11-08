328ec68fae3bf6e2940082da6b25e0e47d965b9a
Bernhart46 <hegyiferenc@proton.me>
2024-11-08 12:46:13 +0100

    Set up routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+import { Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <main className="w-screen h-screen bg-gray-700">
+      <nav></nav>
+      <section className="content">
+        <Routes>
+          <Route path="/" element={<Navigate to="/napi" />} />
+          <Route path="/napi" element={<>Napi</>} />
+          <Route path="/heti" element={<>Heti</>} />
+          <Route path="/havi" element={<>Havi</>} />
+        </Routes>
+      </section>
+    </main>
+  );
 }
 
 export default App;
